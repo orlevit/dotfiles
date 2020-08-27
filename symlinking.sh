6d37cc0408ec820dd5105274c4e416ca06caf4b0
Or Levitas <orlevitas@gmail.com>
2020-08-27 06:14:02 +0200

    change symlink script to remove exsiting files

--- symlinking.sh
+++ symlinking.sh
@@ -1,30 +1,18 @@
 !/usr/bin/env bash
-
-#declare -a INCLUDING=(".bash_profile" ".bashrc" ".aliases" ".vimrc" ".functions")
-##for file in ${INCLUDING[@]}; do
-#	ln -sv ~/Projects/dotfiles/$file ~
-#	#echo "~/Projects/dotfiles/$file" 
-#done
-export PATH_TO_DEV="$HOME/"
-
 PROMPT='[Boostrap]'
-
-# Initialize a few things
-init () {
-	echo "$PROMPT Making a Projects folder in $PATH_TO_DEV if it doesn't already exist"
-	mkdir -p "$PATH_TO_DEV"
-}
 
 # Link the files in $HOME to the dotfile in the directory
 link () {
-	echo "$PROMPT This utility will symlink the files in this repo to the home directory"
+	echo "$PROMPT This utility do:"
+	echo "$PROMPT 1. Will delete the dot files in home directory" 
+	echo "$PROMPT 2. Symlink the files in this repo to the home directory"
 	echo "$PROMPT Proceed? (y/n)"
 	read resp
 	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
 		for file in $(ls -A|grep -E '^\.'|grep -Ev '.git$|*.md') ; do
-			ln -sv "$PWD/$file" "$HOME"
+			[ -f "$HOME/$file" ] && rm  "$HOME/$file"
+			ln -sv "$pwd/$file" "$HOME"
 		done
-		# TODO: source files here?
 		echo "$PROMPT Symlinking complete"
 	else
 		echo "$PROMPT Symlinking cancelled by user"
@@ -32,5 +20,4 @@ link () {
 	fi
 }
 
-init
 link
