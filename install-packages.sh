964305c8f2097efc8ad419c20f2190fb520ac202
Or Levitas <orlevitas@gmail.com>
2020-08-31 13:41:44 +0200

    added vundle installation and better logs

--- install-packages.sh
+++ install-packages.sh
@@ -4,30 +4,50 @@
 source .exports
 
 # Create directory for packages if it doesn't exists
-mkdir -p PATH_TO_ADDITIONAL_PACKAGES 
-cd PATH_TO_ADDITIONAL_PACKAGES
-
+mkdir -p $PATH_TO_ADDITIONAL_PACKAGES 
+echo "Installation Log for date: $(date)" > $LOG_FILE
 # ----------------------
 # Git installation 
 # ----------------------
 
 # autojump
-git clone git://github.com/wting/autojump.git
-cd autojump && . install.py 
-
 if type -p  autojump > /dev/null; then
-    echo "autojump Installed" >> $LOG_FILE
+	echo "autojump already installed" >> $LOG_FILE
 else
-    echo "autojump FAILED TO INSTALL!!!" >> $LOG_FILE
+	git clone git://github.com/wting/autojump.git $PATH_TO_ADDITIONAL_PACKAGES
+	cd autojump && . install.py 
+
+	if type -p  autojump > /dev/null; then
+	    echo "autojump Installed" >> $LOG_FILE
+	else
+	    echo "autojump FAILED TO INSTALL!!!" >> $LOG_FILE
+	fi
 fi
 
 # fuzzy finder
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
 if type -p "fzf" > /dev/null; then
-    echo "fuzzy finder Installed" >> $LOG_FILE
+	echo "fuzzy finder already installed" >> $LOG_FILE
 else
-    echo "fuzzy finder FAILED TO INSTALL!!!" >> $LOG_FILE
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf $PATH_TO_ADDITIONAL_PACKAGES
+	~/.fzf/install
+
+	if type -p "fzf" > /dev/null; then
+	    echo "fuzzy finder Installed" >> $LOG_FILE
+	else
+	    echo "fuzzy finder FAILED TO INSTALL!!!" >> $LOG_FILE
+	fi
 fi
 
+# vim plug-ins
+# Vundle - package manager
+if [[ -d ~/.vim/bundle ]]; then
+	echo "Vundle  already installed" >> $LOG_FILE
+else
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+
+	if [[ -d ~/.vim/bundle ]]; then
+	    echo "Vundle Installed" >> $LOG_FILE
+	else
+	    echo "Vundle FAILED TO INSTALL!!!" >> $LOG_FILE
+	fi
+fi
