b02061d9a41fd9c66afd9c01775a29ad2111dbe0
Or Levitas <orlevitas@gmail.com>
2020-10-07 21:34:58 +0300

    added global call to etc/bashrc/

--- disk_alert.sh
+++ disk_alert.sh
@@ -1,10 +1,10 @@
  #!/bin/bash
 
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
-THRESHOLD=90
+THRESHOLD=10
 
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
     mail -s 'Disk Space Alert' orlevitas@gmail.com << EOF
-Your root partition remaining free space is critically low. Used: $CURRENT%
+Openu: Your root partition remaining free space is critically low. Used: $CURRENT%
 EOF
 fi
