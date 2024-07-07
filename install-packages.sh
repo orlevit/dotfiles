17b2dd3456979f54864c07b27a7cc28a0fb2db90
orlevit <49340082+orlevit@users.noreply.github.com>
2024-07-07 12:07:11 +0300

    Update install-packages.sh
    Added Vim installations:
    
    vim-plug
    vim-surround

--- install-packages.sh
+++ install-packages.sh
@@ -1,53 +1,79 @@
-#!/user/bin/env bash
-# Installation of additional packages, that make life easier 
+#!/usr/bin/env bash
+# Installation of additional packages, that make life easier
 
 source .exports
 
-# Create directory for packages if it doesn't exists
+# Create directory for packages if it doesn't exist
 mkdir -p $PATH_TO_ADDITIONAL_PACKAGES 
 echo "Installation Log for date: $(date)" > $LOG_FILE
+
 # ----------------------
 # Git installation 
 # ----------------------
 
 # autojump
-if type -p  autojump > /dev/null; then
-	echo "autojump already installed" >> $LOG_FILE
+if type -p autojump > /dev/null; then
+    echo "autojump already installed" >> $LOG_FILE
 else
-	git clone git://github.com/wting/autojump.git $PATH_TO_ADDITIONAL_PACKAGES
-	cd autojump && . install.py 
+    git clone git://github.com/wting/autojump.git $PATH_TO_ADDITIONAL_PACKAGES/autojump
+    cd $PATH_TO_ADDITIONAL_PACKAGES/autojump && ./install.py 
 
-	if type -p  autojump > /dev/null; then
-	    echo "autojump Installed" >> $LOG_FILE
-	else
-	    echo "autojump FAILED TO INSTALL!!!" >> $LOG_FILE
-	fi
+    if type -p autojump > /dev/null; then
+        echo "autojump Installed" >> $LOG_FILE
+    else
+        echo "autojump FAILED TO INSTALL!!!" >> $LOG_FILE
+    fi
+    cd -
 fi
 
 # fuzzy finder
 if type -p "fzf" > /dev/null; then
-	echo "fuzzy finder already installed" >> $LOG_FILE
+    echo "fuzzy finder already installed" >> $LOG_FILE
 else
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf $PATH_TO_ADDITIONAL_PACKAGES
-	~/.fzf/install
+    git clone --depth 1 https://github.com/junegunn/fzf.git $PATH_TO_ADDITIONAL_PACKAGES/fzf
+    $PATH_TO_ADDITIONAL_PACKAGES/fzf/install
 
-	if type -p "fzf" > /dev/null; then
-	    echo "fuzzy finder Installed" >> $LOG_FILE
-	else
-	    echo "fuzzy finder FAILED TO INSTALL!!!" >> $LOG_FILE
-	fi
+    if type -p "fzf" > /dev/null; then
+        echo "fuzzy finder Installed" >> $LOG_FILE
+    else
+        echo "fuzzy finder FAILED TO INSTALL!!!" >> $LOG_FILE
+    fi
 fi
 
-# vim plug-ins
-# Vundle - package manager
-if [[ -d ~/.vim/bundle ]]; then
-	echo "Vundle  already installed" >> $LOG_FILE
+# vim-plug - plugin manager
+if [[ -f ~/.vim/autoload/plug.vim ]]; then
+    echo "Vim-Plug already installed" >> $LOG_FILE
 else
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	if [[ -d ~/.vim/bundle ]]; then
-	    echo "Vundle Installed" >> $LOG_FILE
-	else
-	    echo "Vundle FAILED TO INSTALL!!!" >> $LOG_FILE
-	fi
+    if [[ -f ~/.vim/autoload/plug.vim ]]; then
+        echo "Vim-Plug Installed" >> $LOG_FILE
+    else
+        echo "Vim-Plug FAILED TO INSTALL!!!" >> $LOG_FILE
+    fi
+fi
+
+# Add vim-surround to .vimrc and install plugins
+VIMRC=~/.vimrc
+
+if ! grep -q "Plug 'tpope/vim-surround'" "$VIMRC"; then
+    # Add vim-plug block if not present
+    if ! grep -q "call plug#begin('~/.vim/plugged')" "$VIMRC"; then
+        echo "Adding vim-plug configuration to $VIMRC"
+        echo "
+call plug#begin('~/.vim/plugged')
+Plug 'tpope/vim-surround'
+call plug#end()
+" >> "$VIMRC"
+    else
+        echo "Adding vim-surround to existing plug configuration in $VIMRC"
+        sed -i "/call plug#begin('~\/.vim\/plugged')/a\Plug 'tpope/vim-surround'" "$VIMRC"
+    fi
+
+    # Install plugins
+    vim +PlugInstall +qall
+    echo "vim-surround installed" >> $LOG_FILE
+else
+    echo "vim-surround already configured in $VIMRC" >> $LOG_FILE
 fi
