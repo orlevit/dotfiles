df635df271836bb947c3a79f4ad2bf344cfe5ac4
Or Levitas <orlevitas@gmail.com>
2024-07-08 21:48:33 +0300

    added

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,34 @@
 local lazypath='/home/or/dev/dotfiles/Additional_packages/lazy.vim'
 
+------ Nvim configurations -----
+vim.g.mapleader = ","               -- Set the leader key to comma
+vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+
+-- Tab
+vim.opt.tabstop = 4                 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4             -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
+vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+
+-- UI config
+vim.opt.number = true               -- show absolute number
+vim.opt.relativenumber = true       -- add numbers to each line on the left side
+vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+
+-- Better window navigation
+
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+----------------------------------
+
 ------- Install Lazy ------
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -19,19 +48,44 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-	  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	  {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }}
+	     {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+         {"nvim-treesitter/nvim-treesitter",
+   	       build = ":TSUpdate",
+   	       config = function () 
+   	         local configs = require("nvim-treesitter.configs")
+
+   	         configs.setup({
+   	             ensure_installed = { "lua", "python" },
+   	             sync_install = false,
+   	             highlight = { enable = true },
+   	             indent = { enable = true },  
+   	           })
+   	       end},
+ {"catppuccin/nvim", name = "catppuccin", priority = 1000 }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
 -------------------------------------------------
+-- Add parppuccin coloescheme
+-------------------------------------------------
+require("catppuccin").setup()
+vim.cmd[[colorscheme catppuccin]]
+
+-------------------------------------------------
+-- telescope recommanded bindings
+-------------------------------------------------
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
