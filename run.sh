48af16e694e57a5e1b251c2cd2215267af9d154f
Or Levitas <orlevitas@gmail.com>
2024-07-13 19:47:49 +0300

    add

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-source install-packages.sh
-source vim_or_nvim_installation.sh
-source symlinking.sh
-source post_installation.sh
+source bash_scripts/install-packages.sh
+source bash_scripts/vim_or_nvim_installation.sh
+source bash_scripts/symlinking.sh
+source bash_scripts/post_installation.sh
 
 echo "Finished!" >> $LOG_FILE
