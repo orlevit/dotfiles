f7768bf318be8b5e818dbea86c0090e32fcc65e4
Or Levitas <orlevitas@gmail.com>
2024-07-13 17:47:46 +0300

    add text object selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,22 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+                 enable = true,
+                 keymaps = {
+                   init_selection = "<C-space>",
+                   node_incremental = "<C-space>",
+                   scope_incremental = false,
+                   node_decremental = "<bs>",
+                 },
+        },
       })
     end
   }
