88b8bf2c391663230ec0a0f98232c8202deeaebf
Or Levitas <orlevitas@gmail.com>
2024-09-07 16:19:11 +0300

    fix formatters of python

--- dotfiles_to_link/.config/nvim/lua/plugins/none-ls.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,12 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.black,
+        --null_ls.builtins.diagnostics.erb_lint,
+        --null_ls.builtins.diagnostics.rubocop,
+        --null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.pylint,
       },
     })
 
