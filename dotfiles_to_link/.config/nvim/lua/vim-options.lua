3ebcc2f7ebe5dd5022da09cadeb78e142c5aeffe
Or Levitas <orlevitas@gmail.com>
2025-01-06 19:02:12 +0200

    toggle highlight search

--- dotfiles_to_link/.config/nvim/lua/vim-options.lua
+++ dotfiles_to_link/.config/nvim/lua/vim-options.lua
@@ -13,6 +13,9 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>h', function()
+  vim.o.hlsearch = not vim.o.hlsearch
+end, { desc = "Toggle search highlighting" })
+
 vim.wo.number = true
 vim.wo.relativenumber = true
