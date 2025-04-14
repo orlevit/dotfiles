4cf6af71f7e2eea3d355200a6b740222aee1adff
Or Levitas <orlevitas@gmail.com>
2025-04-15 01:07:12 +0300

    change keymaps

--- dotfiles_to_link/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/telescope.lua
@@ -15,10 +15,10 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set("n", "<leader>k", builtin.keymaps, {})
+      vim.keymap.set("n", "<leader>tf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>tg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>to", builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>tk", builtin.keymaps, {})
 
       require("telescope").load_extension("ui-select")
     end,
