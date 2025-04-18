97aa8f1e4c8674db5b9353f9f5955812aabb169b
Or Levitas <orlevitas@gmail.com>
2025-04-18 19:35:43 +0300

    change telescope keymaps; add auto-session

--- dotfiles_to_link/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,38 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>tf", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>tg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>to", builtin.oldfiles, {})
-      vim.keymap.set("n", "<leader>tk", builtin.keymaps, {})
+      },
+    })
 
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+    telescope.load_extension("fzf")
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>tf", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+      vim.keymap.set("n", "<leader>tg", builtin.live_grep, { desc = "Find string in cwd" })
+      vim.keymap.set("n", "<leader>to", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+      vim.keymap.set("n", "<leader>tc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+      vim.keymap.set("n", "<leader>tk", builtin.keymaps, { desc = "Find keymaps" })
+  end,
 }
