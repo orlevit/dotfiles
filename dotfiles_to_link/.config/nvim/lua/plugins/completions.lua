001c8c8dd4aa865061333351ff2a86049dda1bd9
Or Levitas <orlevitas@gmail.com>
2025-04-20 00:46:56 +0300

    chag keymap conflict with tmux

--- dotfiles_to_link/.config/nvim/lua/plugins/completions.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/completions.lua
@@ -31,7 +31,7 @@ return {
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-c>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
