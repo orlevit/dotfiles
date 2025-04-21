67bcce5ba230734e03318add41dd15d5b47b8e76
Or Levitas <orlevitas@gmail.com>
2025-04-22 01:19:20 +0300

    add plugins

--- dotfiles_to_link/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/treesitter.lua
@@ -3,24 +3,38 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "windwp/nvim-ts-autotag" },
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
-        ensure_installed = {"python", "lua", "html", "markdown","json","bash","gitignore", "markdown", "markdown_inline"},
+        autotag = { enable = true }, -- enable autotagging (w/ nvim-ts-autotag plugin)
+        ensure_installed = {
+          "python",
+          "lua",
+          "html",
+          "yaml",
+          "dockerfile",
+          "vim",
+          "markdown",
+          "json",
+          "bash",
+          "gitignore",
+          "markdown",
+          "markdown_inline",
+        },
         incremental_selection = {
-                 enable = true,
-                 keymaps = {
-                       init_selection = "<leader>v",        -- start selection
-                       node_incremental = "u",     -- increment node
-                       scope_incremental = false,   -- increment scope
-                       node_decremental = "m",          -- decrement node
-                 },
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>v", -- start selection
+            node_incremental = "u", -- increment node
+            scope_incremental = false, -- increment scope
+            node_decremental = "m", -- decrement node
+          },
         },
       })
     end,
-  }
+  },
 }
