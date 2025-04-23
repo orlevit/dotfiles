ab0a8a4b9faeb254fc463e708998e7365c479a8b
Or Levitas <orlevitas@gmail.com>
2025-04-24 00:37:16 +0300

    change keymaps

--- dotfiles_to_link/.config/nvim/lua/plugins/trouble.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/trouble.lua
@@ -6,7 +6,7 @@ return {
 	},
 	cmd = "Trouble",
 	keys = {
-		{ "<leader>xo", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
+		{ "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
 		{
 			"<leader>xd",
 			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
