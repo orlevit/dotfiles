d4657f70a4e00365005f6a8bc5a4094dd488d2fa
Or Levitas <orlevitas@gmail.com>
2025-07-21 22:11:50 +0300

    added copilot.lua which-key.nvim.lua and change the cmp to always be on the top of the line

---
+++ dotfiles_to_link/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    build = ":Copilot auth",
+    config = function()
+      local cmp = require("cmp")
+
+      -- Setup copilot
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<Tab>",
+            dismiss = "<C-e>",
+            next = "<M-j>",
+            prev = "<M-k>",
+          },
+        },
+        panel = {
+          enabled = true,
+        },
+        filetypes = {
+          markdown = true,
+          help = true,
+        },
+      })
+      --
+      -- -- Set cmp placement dynamically
+      -- local function set_cmp_placement(pos)
+      --         local current_config = cmp.get_config()
+      --         local new_cop_config = current_config
+      --         new_cop_config.window.completion.placement = pos
+      --         cmp.setup(new_cop_config)
+      --       end
+      --
+      -- set_cmp_placement("top")
+
+    end,
+  },
+}
