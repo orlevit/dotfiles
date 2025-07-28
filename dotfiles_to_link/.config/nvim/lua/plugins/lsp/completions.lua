ab11a501e558d804e9e4212d654b9ad3cd031a2e
Or Levitas <orlevitas@gmail.com>
2025-07-28 13:35:31 +0300

    move to folder lsp

--- dotfiles_to_link/.config/nvim/lua/plugins/lsp/completions.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/lsp/completions.lua
@@ -17,66 +17,30 @@ return {
       local cmp = require("cmp")
       local lspkind = require("lspkind")
       require("luasnip.loaders.from_vscode").lazy_load()
-
+      
       cmp.setup({
         completion = { -- Control the popup-menu behavior
           completeopt = "menu,menuone,preview,noselect",
         },
         snippet = { -- configure how nvim-cmp interacts with snippet engine
-          expand = function(args)
+          expand = function(args) 
             require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
-          completion = cmp.config.window.bordered(),
-          -- completion.zindex = 2001,
-          documentation = cmp.config.window.bordered(),
+          completion = {
+            border = "rounded",
+            zindex = 20001,  -- this ensures popup is on top
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+            scrolloff = 0,
+            col_offset = 0,
+            side_padding = 1,
+          },
+          documentation = {
+            border = "rounded",
+            zindex = 20000,  -- slightly below the menu
+          },
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_next_item(),
-          ["<C-j>"] = cmp.mapping.select_prev_item(),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-c>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path"   },
-          { name = "buffer" },
-        }),
-        formatting = { -- For vs-code like pictograms in completion menu
-          format = lspkind.cmp_format({
-            maxwidth = 50,
-            ellipsis_char = "...",
-          }),
-        },
-      })
-      
-    -- Due to Copilot, needed changes 
-    -- Because of Copliot, we need to ensure that the completion menu is always on top
-    -- if the completion menu is not on top , increase the  "zindex" value.
-    cmp.setup({
-      window = {
-        completion = {
-          border = "rounded",
-          zindex = 20001,  -- this ensures popup is on top
-          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-          scrolloff = 0,
-          col_offset = 0,
-          side_padding = 1,
-        },
-        documentation = {
-          border = "rounded",
-          zindex = 20000,  -- slightly below the menu
-        },
-      },
-    })
-
-    -- the first suggestion it fro the buttom
-    cmp.setup({
         view = {
           docs = {
             auto_open = true,
@@ -84,8 +48,33 @@ return {
           entries = {
             follow_cursor = false,
             name = "custom",
-            selection_order = "bottom_up", -- this starts selection from bottom
+            selection_order =  "top_down", --"bottom_up", -- "top_down", -- this starts selection from top
           },
+        },
+        experimental = {
+          ghost_text = false, -- disable ghost text to avoid conflicts
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-c>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        -- Sources for autocomplete ( Order matters!)
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path"   },
+        }),
+        formatting = { -- For vs-code like pictograms in completion menu
+          format = lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
         },
       })
     end,
