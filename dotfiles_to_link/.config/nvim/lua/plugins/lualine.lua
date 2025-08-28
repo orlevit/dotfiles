c3cd74ee9693ddf651485732a5217c851cb74135
Or Levitas <orlevitas@gmail.com>
2025-08-28 22:32:38 +0300

    pulgin chages , add pull request comment view in CLI

--- dotfiles_to_link/.config/nvim/lua/plugins/lualine.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/lualine.lua
@@ -1,71 +1,76 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-    local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
-      inactive_fg = "#dddddd" -- a brighter mid-gray
-    }
+		local colors = {
+			blue = "#65D1FF",
+			green = "#3EFFDC",
+			violet = "#FF61EF",
+			yellow = "#FFDA7B",
+			red = "#FF4A4A",
+			fg = "#c3ccdc",
+			bg = "#112638",
+			inactive_bg = "#2c3043",
+			inactive_fg = "#dddddd", -- a brighter mid-gray
+		}
 
-    local my_lualine_theme = {
-      normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      insert = {
-        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      command = {
-        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.inactive_fg, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.inactive_fg },
-        c = { bg = colors.inactive_bg, fg = colors.inactive_fg },
-      },
-    }
+		local my_lualine_theme = {
+			normal = {
+				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.bg, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			insert = {
+				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.bg, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			visual = {
+				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.bg, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			command = {
+				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.bg, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			replace = {
+				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.bg, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			inactive = {
+				a = { bg = colors.inactive_bg, fg = colors.inactive_fg, gui = "bold" },
+				b = { bg = colors.inactive_bg, fg = colors.inactive_fg },
+				c = { bg = colors.inactive_bg, fg = colors.inactive_fg },
+			},
+		}
 
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        theme = my_lualine_theme,
-      },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          -- { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				theme = my_lualine_theme,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" }, -- put branch in b
+				lualine_c = {
+					{ "filename", path = 0, shorting_target = 40 }, -- shortens filename if space is tight
+				},
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					-- { "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
