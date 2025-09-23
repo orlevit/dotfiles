9f7141f62e5fc33b2d06eb13143946c7981bc573
Or Levitas <orlevitas@gmail.com>
2025-09-23 21:41:45 +0300

    add molten  options and andd jupynvim

--- dotfiles_to_link/.config/nvim/lua/plugins/jupynium.nvim.lua
+++ dotfiles_to_link/.config/nvim/lua/plugins/jupynium.nvim.lua
@@ -17,10 +17,11 @@ return {
     local opts = { silent = true, noremap = true }
 
     map("n", "<leader>js", "<Cmd>JupyniumStartAndAttachToServer<CR>", vim.tbl_extend("force", opts, { desc = "Start Jupynium" }))
-    map("n", "<C-CR>", "<Cmd>JupyniumExecuteSelectedCells<CR>", vim.tbl_extend("force", opts, { desc = "Execute selected cells" }))
+    map("n", "<leader>jj", "<Cmd>JupyniumExecuteSelectedCells<CR>", vim.tbl_extend("force", opts, { desc = "Execute selected cells" }))
     map("n", "<leader>ja", "<Cmd>JupyniumExecuteAllCells<CR>", vim.tbl_extend("force", opts, { desc = "Execute all cells" }))
     map("n", "<leader>jc", "<Cmd>JupyniumClearOutputs<CR>", vim.tbl_extend("force", opts, { desc = "Clear outputs" }))
     map("n", "<leader>jr", "<Cmd>JupyniumRestartKernel<CR>", vim.tbl_extend("force", opts, { desc = "Restart kernel" }))
     map("n", "<leader>ji", "<Cmd>JupyniumInterruptKernel<CR>", vim.tbl_extend("force", opts, { desc = "Stop kernel" }))
+    map("n", "<leader>jy", "<Cmd>JupyniumStartSync<CR>", vim.tbl_extend("force", opts, { desc = "Sync" }))
   end,
 }
